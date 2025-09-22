4a0f4cc314a1b06eb502f908dde396fead51ba90
Pedro <pedroausqui@hotmail.com>
2025-09-22 19:18:48 -0300

    LogIn Design Update

---
+++ LogIn-SignUp-Page/Script.js
@@ -0,0 +1,12 @@
+
+function togglePass() {
+  const input = document.getElementById("password");
+  if (input.type == "password"){
+
+    input.type = "text";
+  } else {
+
+    input.type = "password";
+  }
+
+}
